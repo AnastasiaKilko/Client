23206ec1ae91633383b7dbb8c834609a6eec5510
public <public@copp.local>
2025-02-18 17:31:09 +0700

    computed stuff p47

--- js/main.js
+++ js/main.js
@@ -2,24 +2,25 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks.",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=sock",
-        inStock: false,
-        inventory: 0,
-        onSale: false,
+        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 15
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -32,8 +33,26 @@ let app = new Vue({
         removeFromCart() {
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant] .variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant] .variantQuantity;
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' — on sale!';
+            }
+            return this.brand + ' ' + this.product + ' — not on sale now';
         }
     }
 })
